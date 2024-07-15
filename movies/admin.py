67bfc2f38c6d53c503cf17dc1d0408954426baac
Jayakrishnanpdm <jayakrishnanmanupdm@gmail.com>
2024-07-15 17:43:56 +0530

    modifications

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from . models import movie_info,Directors
+from . models import movie_info,Directors,Actors,Censor_info
 
 # Register your models here.
 admin.site.register(movie_info)
 admin.site.register(Directors)
+admin.site.register(Actors)
+admin.site.register(Censor_info)
